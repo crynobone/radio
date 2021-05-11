72fdcc1220481073edbf9ebbd0c829f527c1d10b
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-12 00:40:59 +0100

    feature: add $aerial.errors object

--- resources/js/aerial.js
+++ resources/js/aerial.js
@@ -12,12 +12,32 @@ Aerial.mount = function (component, state = {}, methods = [], route = '/aerial/c
         }, {}),
         $aerial: {
             processing: false,
+            errors: {
+                store: {},
+                any() {
+                    return Object.values(this.store).length > 0
+                },
+                all() {
+                    return this.store
+                },
+                get(key) {
+                    return this.store[key]
+                },
+                has(key) {
+                    return this.store[key] !== undefined
+                },
+                reset() {
+                    this.store = {}
+                }
+            }
         }
     }
 }
 
 Aerial.call = function (component, method, route) {
     return async function (...args) {
+        this.$aerial.errors.reset()
+
         this.$aerial.processing = true
 
         const state = Object.fromEntries(Object.entries(this).filter(entry => {
@@ -46,6 +66,11 @@ Aerial.call = function (component, method, route) {
         }).then(async res => {
             const response = await res.json()
 
+            if (! res.ok && response.errors) {
+                this.$aerial.errors.store = response.errors
+                return res
+            }
+
             Object.entries(response.state).forEach(entry => {
                 const [key, value] = entry
 
@@ -55,6 +80,8 @@ Aerial.call = function (component, method, route) {
             this.$aerial.processing = false
 
             return response.result
+        }).catch(error => {
+            console.log(error)
         });
     }
 }
