510dae15909d807ac76490c635153421e1e0a810
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-12 15:49:31 +0100

    tests: setup package

--- tests/Browser/TestCase.php
+++ tests/Browser/TestCase.php
@@ -1,8 +1,21 @@
 <?php
 
+namespace Aerial\Tests\Browser;
+
+use Aerial\AerialServiceProvider;
 use Orchestra\Testbench\Dusk\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function getPackageProviders($app)
+    {
+        return [
+            AerialServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $this->app['config']->set('database.default', 'sqlite');
+    }
 }
